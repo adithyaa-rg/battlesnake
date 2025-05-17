9463f2a994c45bf1c575e483db911c1528556eb6
adithyaa-rg <adithyaa2003@gmail.com>
2025-05-17 15:47:28 +0200

    some policies within the new trained set seems to be pretty decent - atleast greedy for food and survive pretty long. Have to identify which checkpoint it is. Most of the policies have a 50-50 win rate, so thats still good

--- ppo_battlesnake_stochastic.py
+++ ppo_battlesnake_stochastic.py
@@ -838,9 +838,9 @@ def main():
         # "ckpt_dir": "./models/PPO_BattleSnake_NEW_WIN_LOSS_PENALTIES_FOR_DEATH_AND_REWARDS_FOR_KILLS",
         "ckpt_dir": "./models/PPO_BattleSnakes_go_offense_no_contact",
         # Example: "./models/PPOBattlesnake_Corrected/agent_0_steps_50000.pth"
-        "load_model_path_agent0": None, # Path for agent 0
-        "load_model_path_agent1": None, # Path for agent 1
-        "render_mode": False, # Set to True to watch
+        "load_model_path_agent0": "./models/PPO_BattleSnakes_go_offense_no_contact/agent_0_steps_2900000.pth", # Path for agent 0
+        "load_model_path_agent1": "./models/PPO_BattleSnakes_go_offense_no_contact/agent_1_steps_2900000.pth", # Path for agent 1
+        "render_mode": True, # Set to True to watch
         "render_freq": 0.5, # Time in seconds between rendered frames, e.g., 0.1 for 10 FPS
     }
     """
